534dd250d05ddf75bb2fee8e985333ac6d46a599
Vinicius Bonito <franca.vinicius@outlook.com>
2020-12-04 23:53:49 -0300

    Atualização mundo 1
    Atualização do ex001 e resolução do ex002

--- Mundo 1/ex001.py
+++ Mundo 1/ex001.py
@@ -1,3 +1,4 @@
 # Crie um programa que escreva 'Olá, Mundo na tela'
 
-print('Olá, mundo!')
+mensagem = ('Olá, mundo!')
+print(mensagem)
---
+++ Mundo 1/ex002.py
@@ -0,0 +1,4 @@
+# Crie um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas
+
+nome = input('Digite seu nome: ')
+print('Olá {}, seja muito bem vindo!'.format(nome))
